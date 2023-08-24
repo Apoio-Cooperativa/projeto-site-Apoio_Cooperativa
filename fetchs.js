c976530224e61a711de0029e2ac488011b708108
Paulo Cavalcante <ph.doamaralcavalcante@gmail.com>
2023-08-23 23:00:15 -0300

    testando o fetch

--- fetchs.js
+++ fetchs.js
@@ -1,7 +1,20 @@
-function fetchs() {
-    var nav = document.getElementById("nav")
-    var navbar = "navbar.html"
-    fetch(navbar)
+onload = () => {
+    const url = "navbar/navbar.html",
+        url2 = "footer/footer.html";
+    var nav = document.getElementById("nav"),
+        footer = document.getElementById("footer");
+
+    fetch(url)
         .then(res => res.text())
         .then(text => nav.innerHTML = text)
+        .catch(error => {
+            console.error("Ocorreu um erro:", error);
+        });
+
+    fetch(url2)
+        .then(res => res.text())
+        .then(text => footer.innerHTML = text)
+        .catch(error => {
+            console.error("Ocorreu um erro:", error);
+        });
 }
