8cc773e0580ae2e940e4c73ed6f77188111e59e1
Paulo Cavalcante <ph.doamaralcavalcante@gmail.com>
2023-09-01 11:38:03 -0300

    teste
    isso q eu fiz é meio arriscado, mas chefe é chefe, né pae

---
+++ fetchs.js
@@ -0,0 +1,73 @@
+/* let id;
+
+const verif = () => {
+    if (sessionStorage.getItem('reloaded')) {
+        
+    }
+    sessionStorage.setItem('reloaded', 'true');
+}
+ */
+const nav = () => {
+    const url = "navbar/navbar.html",
+        nav = document.getElementById("nav");
+
+    fetch(url)
+        .then(res => res.text())
+        .then(text => nav.innerHTML = text)
+}
+
+const inicio = () => {
+    const url = "inicio.html",
+        body = document.getElementById("corpo");
+
+    fetch(url)
+        .then(res => res.text())
+        .then(text => body.innerHTML = text)
+        .catch(error => {
+            body.innerHTML = "<p>Página indisponével</p>"
+        });
+
+    id = "inicio";
+}
+
+const doacao = () => {
+    const url = "doacao.html",
+        body = document.getElementById("corpo");
+
+    fetch(url)
+        .then(res => res.text())
+        .then(text => body.innerHTML = text)
+
+    id = "doacao";
+}
+
+const horarios = () => {
+    const url = "horarios.html",
+        body = document.getElementById("corpo");
+
+    fetch(url)
+        .then(res => res.text())
+        .then(text => body.innerHTML = text)
+
+    id = "horarios"
+}
+
+const reciclar = () => {
+    const url = "comoReciclar.html",
+        body = document.getElementById("corpo");
+
+    fetch(url)
+        .then(res => res.text())
+        .then(text => body.innerHTML = text)
+
+    id = "reciclar";
+}
+
+const foot = () => {
+    const url = "footer/footer.html",
+        footer = document.getElementById("footer");
+
+    fetch(url)
+        .then(res => res.text())
+        .then(text => footer.innerHTML = text)
+}
