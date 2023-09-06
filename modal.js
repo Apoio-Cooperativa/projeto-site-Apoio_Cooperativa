2b79b800639c143c798bb0a892e2079a10add70c
Paulo Cavalcante <ph.doamaralcavalcante@gmail.com>
2023-09-05 22:16:42 -0300

    Update modal.js

--- modal.js
+++ modal.js
@@ -3,7 +3,6 @@ function closeModal() {
     modal.style.display = 'none';
 }
 
-// Adiciona ouvintes de eventos aos botões e cartões
 function modal () {
     const modal = document.querySelector('.modal');
     modal.style.display = 'flex';
