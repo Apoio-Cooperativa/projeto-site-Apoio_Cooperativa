7c2309bb7cf2752f64174eacb397fdfeacb4d600
Paulo Cavalcante <ph.doamaralcavalcante@gmail.com>
2023-09-06 10:30:05 -0300

    qualquer nome

--- fetchs.js
+++ fetchs.js
@@ -2,8 +2,15 @@ let id;
 
 const verif = () => {
     if (sessionStorage.getItem('reloaded')) {
-
-    }
+        switch (valor) {
+            case ("inicio"): inicio(); break
+            case ("reciclar"): reciclar(); break
+            case ("doacao"): doacao(); break
+            case ("horarios"): horarios(); break
+            //case ("adm"): adm(); break
+            default: alert("tenho que programar ainda!"); inicio()
+        }
+    } else inicio()
     sessionStorage.setItem('reloaded', 'true');
 }
 
