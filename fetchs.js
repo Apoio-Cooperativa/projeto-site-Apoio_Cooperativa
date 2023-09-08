a972effedeeecdeb4bf41f9ec697c4d4baaf56de
Paulo Cavalcante <ph.doamaralcavalcante@gmail.com>
2023-09-07 23:52:02 -0300

    Revert "fetch fixed"
    This reverts commit 637ce78eef8df6583f60c8da3e28b40f2ecf9596.

--- fetchs.js
+++ fetchs.js
@@ -2,13 +2,13 @@ let id;
 
 const verif = () => {
     if (sessionStorage.getItem('reloaded')) {
-        switch (id) {
+        switch (valor) {
             case ("inicio"): inicio(); break
             case ("reciclar"): reciclar(); break
             case ("doacao"): doacao(); break
             case ("horarios"): horarios(); break
             //case ("adm"): adm(); break
-            default: inicio()
+            default: alert("tenho que programar ainda!"); inicio()
         }
     } else inicio()
     sessionStorage.setItem('reloaded', 'true');
