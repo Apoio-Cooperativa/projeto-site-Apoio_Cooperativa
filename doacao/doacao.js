c5f49661af19282731ad58c434cfa8af3b374340
Paulo Cavalcante <ph.doamaralcavalcante@gmail.com>
2023-09-25 12:15:00 -0300

    confirm do botao

--- doacao/doacao.js
+++ doacao/doacao.js
@@ -1,26 +1,40 @@
-/* const confirm = () => {
-    let
-} */
-
-const qrcode = () => {
+const qrcode = (cond) => {
+    let msg = document.getElementById("msg");
     let radios = document.getElementsByName('radio');
     let imagem = document.getElementById('image');
 
-    for (let i = 0; i < radios.length; i++) {
-        if (radios[i].checked) {
-            switch (i) {
-                case 0:
-                    imagem.src = 'img/lindo.jpg';
-                    break;
-                case 1:
-                    imagem.src = 'img/qrcode2.png';
-                    break;
-                case 2:
-                    imagem.src = 'img/sergio.jpg';
-                    break;
-                default:
-                    break;
+    if (cond) {
+        msg.innerHTML = null;
+
+        for (let i = 0; i < radios.length; i++) {
+            if (radios[i].checked) {
+                switch (i) {
+                    case 0:
+                        imagem.src = 'img/lindo.jpg';
+                        break;
+                    case 1:
+                        imagem.src = 'img/qrcode2.png';
+                        break;
+                    case 2:
+                        imagem.src = 'img/sergio.jpg';
+                        break;
+                    default:
+                        break;
+                }
             }
         }
     }
+    else {
+        msg.innerHTML = null;
+        for (let i = 0; i < radios.length; i++) {
+            radios[i].checked = false;
+        }
+    }
+}
+
+const confirm = (valor) => {
+    let msg = document.getElementById("msg");
+    msg.innerHTML = "<p>Deseja doar " + valor + " para a Cooperativa?</p>";
+    msg.innerHTML += "<input type='button' value='Sim' onclick='qrcode(true)'>"
+        + "<input type='button' value='Não' onclick='qrcode(false)'>";
 }
