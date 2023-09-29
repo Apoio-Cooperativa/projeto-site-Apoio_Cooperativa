8e8b28022fde7ddc31b5b1ed68a43d4d5eefa13d
Paulo Cavalcante <ph.doamaralcavalcante@gmail.com>
2023-09-29 11:53:11 -0300

    fazer sumir

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let isAdmin;
 
+//Seta:
+
 const setaAoTopo = document.querySelector('.seta-ao-topo');
 
 setaAoTopo.addEventListener('click', () => {
@@ -8,3 +10,17 @@ setaAoTopo.addEventListener('click', () => {
         behavior: 'smooth'
     });
 });
+
+// Função para verificar a posição de rolagem e mostrar/ocultar a seta
+function verificarPosicaoRolagem() {
+    const setaAoTopo = document.querySelector('.seta-ao-topo');
+    const limiteExibicao = 200; // Define a altura da página em pixels para mostrar a seta
+
+    if (window.scrollY > limiteExibicao) {
+        setaAoTopo.style.display = 'block'; // Mostra a seta quando a rolagem é maior que o limite
+    } else {
+        setaAoTopo.style.display = 'none'; // Oculta a seta quando a rolagem está no topo
+    }
+}
+
+window.addEventListener('scroll', verificarPosicaoRolagem);
