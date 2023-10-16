b4a7779602624584fd01123782eada14f0f53475
felipwz <86151227+felipwz@users.noreply.github.com>
2023-10-16 19:08:55 -0300

    Usei o método do fatch para separar o painel de adm
    (to chegando em algum lugar se pá)

--- pages/adm/adm.js
+++ pages/adm/adm.js
@@ -1,1 +1,8 @@
-let isAdmin;
+const adm = async () => {
+    const url = "admPainel.html",
+        body = document.getElementById("corpo_adm");
+
+    fetch(url)
+        .then(res => res.text())
+        .then(text => body.innerHTML = text)
+}
