282ed02b5a36f805a01323bd120efcc1db64f3ae
Paulo Cavalcante <ph.doamaralcavalcante@gmail.com>
2023-10-17 23:07:25 -0300

    Fazendo dar scroll
    Ao abrir o modal ele dá scroll até ele e ao fechar ele dá scroll ao centro da tela

--- pages/comoReciclar/modal.js
+++ pages/comoReciclar/modal.js
@@ -6,7 +6,9 @@ window.onkeydown = function (event) {
     }
 };
 
-const closeModal = () => {
+const closeModal = async () => {
+    await goCenter()
+    await delay(100)
     const modal = document.getElementById(`modal-${idModal}`);
     triangle(idModal);
     modal.style.display = 'none';
@@ -14,16 +16,35 @@ const closeModal = () => {
 }
 
 const modal = async (id) => {
-    if (id == idModal) return;
+    if (id == idModal) { goModal(id); return; }
     if (!idModal == '') closeModal();
 
     const modal = document.getElementById(`modal-${id}`);
     triangle(id);
     modal.style.display = 'flex';
     idModal = id;
+    goModal(id)
 }
 
 const triangle = (id) => {
     const card = document.getElementById(`card-${id}`);
     card.classList.toggle("show_triangle");
+}
+
+const goModal = (id) => {
+    const modal = document.getElementById(`modal-${id}`);
+    modal.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+        inline: "nearest"
+    });
+}
+
+const goCenter = async () => {
+    const corpo = document.getElementById('corpo_index');
+    corpo.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "nearest"
+    });
 }
