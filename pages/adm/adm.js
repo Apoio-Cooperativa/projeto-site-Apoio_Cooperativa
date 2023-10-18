a3e5930655b7070b1fab893306943ca16a71327c
Paulo Cavalcante <ph.doamaralcavalcante@gmail.com>
2023-10-17 21:36:23 -0300

    apaguei a pasta adm que estava no pages pq n precisaremos mais

--- pages/adm/adm.js
+++
@@ -1,8 +0,0 @@
-const adm = async () => {
-    const url = "painelAdm/admPainel.html",
-        body = document.getElementById("corpo_adm");
-
-    fetch(url)
-        .then(res => res.text())
-        .then(text => body.innerHTML = text)
-}
