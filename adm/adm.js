77754374c5870b708c5d0ae74461101c1272907c
Luiz Felipe <luizfelipesiqueira83@gmail.com>
2023-10-19 09:01:38 -0300

    criação dos horários em adm

--- adm/adm.js
+++ adm/adm.js
@@ -6,3 +6,12 @@ const adm = async () => {
         .then(res => res.text())
         .then(text => body.innerHTML = text)
 }
+
+const doacao = async () => {
+    const url = "pages/horarios/horarios.html..",
+        body = document.getElementById("corpo_adm");
+
+    fetch(url)
+        .then(res => res.text())
+        .then(text => body.innerHTML = text)
+}
