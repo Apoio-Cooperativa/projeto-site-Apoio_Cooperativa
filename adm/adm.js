462f3c71fa6ae3c95a3c7c514eab87589cea3b3c
Luiz Felipe <luizfelipesiqueira833@gmail.com>
2023-10-20 11:27:44 -0300

    Adicionando a tela de horários em adm
    uma copia da página de horários para adm

--- adm/adm.js
+++ adm/adm.js
@@ -1,15 +1,15 @@
-const adm = async () => {
+const paineladm = async () => {
     const url = "painelAdm/admPainel.html",
-        body = document.getElementById("corpo_adm");
+        body = document.getElementById("painel_adm");
 
     fetch(url)
         .then(res => res.text())
         .then(text => body.innerHTML = text)
 }
 
-const doacao = async () => {
-    const url = "adm/horariosAdm/horarios.html",
-        body = document.getElementById("corpo_adm");
+const horariosadm = async () => {
+    const url = "horariosAdm/horariosAdm.html",
+        body = document.getElementById("horarios_adm");
 
     fetch(url)
         .then(res => res.text())
