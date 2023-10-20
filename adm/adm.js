d861d4ad89bf39908cd6f60f7ee2847581e04f0a
Luiz Felipe <luizfelipesiqueira833@gmail.com>
2023-10-20 11:39:05 -0300

    adicionando js no adm.js

--- adm/adm.js
+++ adm/adm.js
@@ -1,3 +1,22 @@
+const init = () => window.addEventListener('hashchange', renderPage);
+const validateHash = (hash) => hash === "" ? 'inicio' : hash.replace('#', '');
+
+const renderPage = async () => {
+    const page = validateHash(window.location.hash);
+    switch (page) {
+        case ("inicio"): inicio(); break
+        case ("reciclar"): reciclar(); break
+        case ("doacao"): doacao(); break
+        case ("horarios"): horarios(); break
+        // case ("adm"): adm(); break
+        case ("login"): login(); break
+        default: /* inicio() */ lost();
+    }
+    await goUp(100)
+}
+
+
+
 const paineladm = async () => {
     const url = "painelAdm/admPainel.html",
         body = document.getElementById("painel_adm");
