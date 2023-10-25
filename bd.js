c19b5ed2f4709995120f76eec4d5f7d0f5dd4812
Paulo Cavalcante <ph.doamaralcavalcante@gmail.com>
2023-10-24 23:50:43 -0300

    Update bd.js
    quando der erro no banco de dados, ele aparece as tabelas na gambiarra, mas apenas pra não ficar vazio

--- bd.js
+++ bd.js
@@ -1,4 +1,4 @@
-let json = []; 
+let json = [];
 let tbIsRead = true;
 
 const read = async () => {
@@ -13,7 +13,145 @@ const read = async () => {
             },
             error: function (xhr, status, error) {
                 console.error(status + ': ' + error);
+                gamb();
             }
         });
     });
+}
+
+function gamb() {
+    document.getElementById("tables").innerHTML = `<div class="table_horarios_row">
+    <div class="table_horarios_cell">
+        <table>
+            <caption>Alvinópolis</caption>
+            <thead>
+                <tr>
+                    <th>Dia da Semana</th>
+                    <th>Horários</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        terça-feira
+                    </td>
+                    <td>
+                        07:00
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <div class="table_horarios_cell">
+        <table>
+            <caption>Alvinópolis II</caption>
+            <thead>
+                <tr>
+                    <th>Dia da Semana</th>
+                    <th>Horários</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        sábado
+                    </td>
+                    <td>
+                        07:00
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <div class="table_horarios_cell">
+        <table>
+            <caption>Atibaia Jardim</caption>
+            <thead>
+                <tr>
+                    <th>Dia da Semana</th>
+                    <th>Horários</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        terça-feira
+                    </td>
+                    <td>
+                        07:00
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+<div class="table_horarios_row">
+
+    <div class="table_horarios_cell">
+        <table>
+            <caption>Caetetuba</caption>
+            <thead>
+                <tr>
+                    <th>Dia da Semana</th>
+                    <th>Horários</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        segunda-feira
+                    </td>
+                    <td>
+                        18:00
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <div class="table_horarios_cell">
+        <table>
+            <caption>Jardim Cerejeiras</caption>
+            <thead>
+                <tr>
+                    <th>Dia da Semana</th>
+                    <th>Horários</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        segunda-feira
+                    </td>
+                    <td>
+                        18:00
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <div class="table_horarios_cell">
+        <table>
+            <caption>Parque Arco-Íris</caption>
+            <thead>
+                <tr>
+                    <th>Dia da Semana</th>
+                    <th>Horários</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        terça-feira
+                    </td>
+                    <td>
+                        07:00
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+</div>`;
 }
