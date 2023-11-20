d46f6b3efc7f7d3a678239970751f6c49ee36be2
Paulo Cavalcante <ph.doamaralcavalcante@gmail.com>
2023-11-20 14:29:34 -0300

    Avanços no projeto

--- adm/adm.js
+++ adm/adm.js
@@ -83,3 +83,31 @@ const voltar = async () => {
     await delay(100)
     window.location.hash = "#horarios";
 }
+
+const goUp = async (t) => {
+    await delay(t);
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+//Seta:
+
+const verificarPosicaoRolagem = () => {
+    const setaAoTopo = document.getElementById('topo');
+    const limiteExibicao = 0; // Define a altura da página em pixels para mostrar a seta
+
+    if (window.scrollY > limiteExibicao) {
+        setaAoTopo.style.opacity = '1'; // Mostra a seta suavemente
+        setaAoTopo.style.pointerEvents = 'auto'; // Ative a capacidade de clique quando visível
+    } else {
+        setaAoTopo.style.opacity = '0'; // Oculta a seta suavemente
+        setaAoTopo.style.pointerEvents = 'none'; // Desative a capacidade de clique quando invisível
+    }
+    setaAoTopo.addEventListener('click', () => {
+        goUp(0);
+    });
+}
+
+window.addEventListener('scroll', verificarPosicaoRolagem);
